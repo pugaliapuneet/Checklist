054baeea1f0fa2015c6eddf653a1431cbc688f91
Puneet Pugalia <puneet.pugalia@gmail.com>
2020-05-25 20:26:41 +0530

    selecting/deselecting table cell on tap

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -15,9 +15,13 @@ class ChecklistViewController: UITableViewController {
         // Do any additional setup after loading the view.
     }
     
+    //define number of rows
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1000
     }
+    
+    //fill data
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
@@ -37,6 +41,20 @@ class ChecklistViewController: UITableViewController {
         }
         
         return cell
+    }
+    
+    //tick/untick on tap with animated effect
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+            
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
     }
 
 }
