0beeb4bdf972e6b0259f4d782e6cc6abe5536d6d
Puneet Pugalia <puneet.pugalia@gmail.com>
2020-05-26 11:19:49 +0530

    Keyboard controls, unselectable form rows

--- Checklist/AddItemTableViewController.swift
+++ Checklist/AddItemTableViewController.swift
@@ -10,20 +10,58 @@ import UIKit
 
 class AddItemTableViewController: UITableViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
+    @IBOutlet weak var cancelBarButton: UIBarButtonItem!
+    
+    
     @IBAction func cancel(_ sender: Any) {
         navigationController?.popViewController(animated: true)
     }
-    
-    @IBOutlet weak var textField: UITextField!
     
     @IBAction func done(_ sender: Any) {
         navigationController?.popViewController(animated: true)
     }
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
-        
     }
     
+    //popup keyboard automatically
+    override func viewWillAppear(_ animated: Bool) {
+        textField.becomeFirstResponder()
+    }
+    
+    //make rows unselectable
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+}
+
+extension AddItemTableViewController: UITextFieldDelegate {
+    
+    //close the keyboard on return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    //enable add button only if text field is NOT empty
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let oldText = textField.text,
+            let stringRange = Range(range, in: oldText) else {
+            return false
+        }
+        
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        if newText.isEmpty {
+            addBarButton.isEnabled = false
+        } else {
+            addBarButton.isEnabled = true
+        }
+        
+        return true
+    }
 }
